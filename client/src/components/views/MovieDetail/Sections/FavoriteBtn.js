c02d7d5e71a075a3623ba45a1641a84acf238a41
donghyun <dawnme1228@naver.com>
2021-11-24 16:33:29 +0900

    fav button

--- client/src/components/views/MovieDetail/Sections/FavoriteBtn.js
+++ client/src/components/views/MovieDetail/Sections/FavoriteBtn.js
@@ -3,87 +3,21 @@ import { Button } from "antd";
 import Axios from "axios";
 
 function FavoriteBtn(props) {
-  const movieId = props.movieId;
-  const userFrom = props.userFrom;
-  const movieTitle = props.movieInfo.original_title;
-  const moviePost = props.movieInfo.backdrop_path;
-  const movieRuntime = props.movieInfo.runtime;
+  let variables = {
+    userFrom: props.userFrom,
+    movieId: props.movieId,
+    movieTitle: props.movieInfo.original_title,
+    moviePost: props.movieInfo.backdrop_path,
+    movieRuntime: props.movieInfo.runtime,
+  };
 
   const [isFavorite, setIsFavorite] = useState(false);
   const [favoriteNum, setFavoriteNum] = useState(0);
-  const [load, setLoad] = useState(false);
-
-  let variables = {
-    userFrom,
-    movieId,
-    movieTitle,
-    moviePost,
-    movieRuntime,
-  };
-
-  console.log(variables);
-  function onFavHandler(e) {
-    let variables = {
-      userFrom,
-      movieId,
-      movieTitle,
-      moviePost,
-      movieRuntime,
-    };
-    Axios.post("/api/favorite/register", variables).then(() => {
-      Axios.post("/api/favorite/favoriteNum", variables)
-        .then(response => {
-          if (response.data.success) {
-            setFavoriteNum(response.data.favoriteNumber);
-          } else {
-            alert("좋아요한 개수를 불러오는 데 실패했습니다.");
-          }
-        })
-        .then(() => {
-          setIsFavorite(isFavorite ? false : true);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    });
-  }
-
-  function offFavHandler(e) {
-    let variables = {
-      userFrom,
-      movieId,
-      movieTitle,
-      moviePost,
-      movieRuntime,
-    };
-    setLoad(true);
-    Axios.post("/api/favorite/favoriteNum", variables)
-      .then(response => {
-        if (response.data.success) {
-          setFavoriteNum(response.data.favoriteNumber);
-        } else {
-          alert("좋아요한 개수를 불러오는 데 실패했습니다.");
-        }
-      })
-      .then(() => {
-        setIsFavorite(isFavorite ? false : true);
-      })
-      .then(() => setLoad(false))
-      .catch(err => {
-        console.log(err);
-      });
-  }
+  const [load, setLoad] = useState(true);
 
   function onFavHandler(e) {
-    let variables = {
-      userFrom,
-      movieId,
-      movieTitle,
-      moviePost,
-      movieRuntime,
-    };
     setLoad(true);
-    Axios.post("/api/favorite/register", variables)
+    Axios.post("/api/favorite/delete", variables)
       .then(() => {
         return Axios.post("/api/favorite/favoriteNum", variables);
       })
@@ -96,27 +30,62 @@ function FavoriteBtn(props) {
       })
       .then(() => {
         setIsFavorite(isFavorite ? false : true);
+        setLoad(false);
       })
-      .then(() => setLoad(false))
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  function offFavHandler(e) {
+    setLoad(true);
+    Axios.post("/api/favorite/register", variables)
+      .then(() => {
+        return new Promise(resolve => {
+          setTimeout(() => resolve(), 100);
+        });
+      })
+      .then(() => {
+        return Axios.post("/api/favorite/favoriteNum", variables);
+      })
+      .then(response => {
+        if (response.data.success) {
+          setFavoriteNum(response.data.favoriteNumber);
+        } else {
+          alert("좋아요한 개수를 불러오는 데 실패했습니다.");
+        }
+      })
+      .then(() => {
+        setIsFavorite(isFavorite ? false : true);
+        setLoad(false);
+      })
       .catch(err => {
         console.log(err);
       });
   }
 
   useEffect(() => {
-    let variables = {
-      userFrom,
-      movieId,
-      movieTitle,
-      moviePost,
-      movieRuntime,
-    };
     Axios.post("/api/favorite/favoriteNum", variables)
       .then(response => {
         if (response.data.success) {
           setFavoriteNum(response.data.favoriteNumber);
+          setLoad(false);
         } else {
           alert("좋아요한 개수를 불러오는 데 실패했습니다.");
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    Axios.post("/api/favorite/favorited", variables)
+      .then(response => {
+        if (response.data.success) {
+          if (response.data.favorited) {
+            setIsFavorite(true);
+          }
+        } else {
+          alert("정보를 불러오는 데 실패했습니다.");
         }
       })
       .catch(err => {
@@ -127,11 +96,11 @@ function FavoriteBtn(props) {
   return (
     <div style={{ display: "flex", justifyContent: "flex-end" }}>
       {isFavorite ? (
-        <Button disabled={load} type="default" onClick={offFavHandler}>
+        <Button disabled={load} type="default" onClick={onFavHandler}>
           <i className="fas fa-heart"> {favoriteNum}</i>
         </Button>
       ) : (
-        <Button disabled={load} type="default" onClick={onFavHandler}>
+        <Button disabled={load} type="default" onClick={offFavHandler}>
           <i className="far fa-heart"> {favoriteNum}</i>
         </Button>
       )}
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -5,7 +5,7 @@ const { Favorite } = require("../models/Favorite");
 router.post("/favoriteNum", (req, res) => {
   Favorite.find({ movieId: req.body.movieId }, (err, docs) => {
     if (err) {
-      res.status(500).send("database error");
+      res.status(500).send("error");
       console.log(err);
     } else {
       res.status(200).json({
@@ -16,13 +16,52 @@ router.post("/favoriteNum", (req, res) => {
   });
 });
 
+router.post("/favorited", (req, res) => {
+  Favorite.find(
+    { movieId: req.body.movieId, userFrom: req.body.userFrom },
+    (err, docs) => {
+      if (err) {
+        res.status(500).send("error");
+        console.log(err);
+      } else {
+        res.status(200).json({
+          success: true,
+          favorited: docs.length ? true : false,
+        });
+      }
+    }
+  );
+});
+
+router.post("/delete", (req, res) => {
+  Favorite.deleteMany(
+    { movieId: req.body.movieId, userFrom: req.body.userFrom },
+    (err, result) => {
+      if (err) {
+        res.status(500).send("error");
+        console.log(err);
+      } else {
+        res.status(200).json({
+          success: true,
+        });
+      }
+    }
+  );
+});
+
 router.post("/register", (req, res) => {
   let favorite = new Favorite(req.body);
-  favorite.save().then(
-    res.json({
-      success: true,
-    })
-  );
+  favorite
+    .save()
+    .then(
+      res.json({
+        success: true,
+      })
+    )
+    .catch(err => {
+      res.status(500).send("error");
+      console.log(err);
+    });
 });
 
 module.exports = router;
